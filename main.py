b19c18ec2be4ccb0d691d8a3e71ba546b3e74294
Andre07G <eleal087@gmail.com>
2025-04-03 08:55:32 -0500

    feat: :sparkles: Organizacion del main
    Se organizo el main para que el codigo sea funcional, tambien se corrigio un error en los nombres de los archivos
    
    @byAndresLeal

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("PhotoCampus")
+from menues import saludo
+from menu import *
+
+from funciones_varias import mostrar_servicios
+
+
+
+saludo()
+while True:
+    op=menu_principal()
+    match op:
+        case "1":
+            while True:
+                from add_service import añadir_servicio
+                op=reg_servicio()
+                if op=="1": mostrar_servicios()
+                elif op=="2": añadir_servicio()
+                elif op=="3": break
+                else: print("Ingrese una opcion valida")
+        case "2":
+            while True:
+                op=mod_servicio()
+                from edit_services import editar_servicios
+                if op=="1": mostrar_servicios()
+                elif op=="2": editar_servicios()
+                elif op=="3": break
+                else: print("Ingrese una opcion valida")
+
+        case "3":
+            while True:
+                from delete_services import eliminar_servicios
+                op=elim_servicio()
+                if op=="1": mostrar_servicios()
+                elif op=="2": eliminar_servicios()
+                elif op=="3": break
+                else: print("Ingrese una opcion valida")
+        case "4":
+            print("Saliendo")
+            break
+        case _:
+            print("Ingrese una opcion valida")
