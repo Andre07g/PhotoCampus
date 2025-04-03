7c4f141561822fef1ad833ff62c83f7b9f86fe46
KarolainReyes <reyeskarol0412@gmail.com>
2025-04-03 07:30:57 -0500

    feat: :sparkles: Creacion de menus
    Se crea el menu principal y menu para registrar, modificar y eliminar los servicios
    
    @byKarolainReyes

---
+++ menu.py
@@ -0,0 +1,53 @@
+
+
+def menu_principal():
+    menuPrincipal=""""
+    =====================================
+            BIENVENIDO A PHOTOCAMPUS
+    =====================================
+    1. Registrar un nuevo servicio
+    2. Editar un servicio existente
+    3. Eliminar un servicio
+    4. Salir del programa
+    ======================================
+    """
+    print (menuPrincipal)
+    return input ("Por favor seleccione la opcion que desea: ")
+
+
+def reg_servicio():
+    regServicio=""""
+    ======================================
+                 MENU REGISTRO             
+    ======================================
+    1. Ver servicios disponibles
+    2. Registrar un nuevo servicio
+    3. Regresar al menu anterior
+    ======================================
+    """
+    print (regServicio)
+    return input ("Por favor seleccione la opcion que desea: ")
+
+
+def mod_servicio():"""
+    =======================================
+                MENU MODIFICAR
+    =======================================
+    1. Ver servicios disponibles
+    2. Editar un servicio
+    3. Regresar al menu anterior
+    =======================================
+    """
+
+def elim_servicio():
+    elimServicio="""
+    =======================================
+                MENU ELIMINAR
+    =======================================
+    1. Ver servicios disponibles
+    2. Eliminar un servicio
+    3. Regresar al menu anterior
+    =======================================
+    """
+    print (elimServicio)
+    return input ("Por favor ingrese la opcion que desea: ")
